49d5753f90103dc41446c20a22813b237daf04ee
Brian <wamwangimathenge@gmail.com>
2022-11-21 20:08:46 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400) {
+    return "This one is on me!"
+  } else if(distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if(distance > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(199))
+
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous') {
+    return "Thank you so much."
+  } else if(tip === "not as generous") {
+    return 'Thank you.'
+  } else {
+    return "Bye."
+  }
 }
